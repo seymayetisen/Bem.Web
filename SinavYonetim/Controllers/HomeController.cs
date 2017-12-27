47775203ade3a3aa9c850a3dede91c7a3d8865d5
Orhan Aygün <orhan.aygun@outlook.com>
2017-12-27 16:00:49 +0300

    -Entity Framework eklendi

---
+++ SinavYonetim/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using SinavYonetim.Models;
+using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SinavYonetim.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var context = new SinavYonetimDbContext();
+            var personList = context.Person.ToList();
+
+            return View(personList);
+        }
+
+        public ActionResult FilteredResult(string name)
+        {
+            var context = new SinavYonetimDbContext();
+            var personList = context.Person.Where(p=> p.Isim.Contains(name)).ToList();
+
+            return View("~/views/home/index.cshtml",personList);
+        }
+
+        public ActionResult Exam(string name)
+        {
+            var context = new SinavYonetimDbContext();
+            var pe = context.PersonExam.Include(p=> p.Exam).First();
+
+            return View(pe);
+        }
+    }
+}
---
+++ SinavYonetim/Models/PersonsExam.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SinavYonetim.Models
+{
+    public class PersonsExam
+    {
+        public int Id { get; set; }
+        public Person Person { get; set; }
+        public Exam Exam { get; set; }
+        public PersonsExamStatus Status { get; set; }
+        public int Duration { get; set; }
+        public List<Answer> Answers { get; set; }
+    }
+}
